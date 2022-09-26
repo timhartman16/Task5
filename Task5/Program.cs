2885f2b7f4a29bc34a94eba20fb5a5ecdacd611a
timhartman16 <djonatanas@yandex.ru>
2022-09-26 16:03:40 +0300

    Добавьте файлы проекта.

---
+++ Task5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		TransformToElephant();
+		Console.WriteLine("Муха");
+        //... custom application code
+	}
+
+	static void TransformToElephant()
+	{
+		unsafe
+        {
+			string muha = "Муха";
+			fixed (char* ptr = muha)
+            {
+				ptr[0] = 'C';
+				ptr[1] = 'л';
+				ptr[2] = 'о';
+				ptr[3] = 'н';
+			}
+		}
+	}
+}
